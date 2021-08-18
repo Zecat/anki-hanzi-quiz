3d964944dfef504369ddb4058b1d6673e2f2faf8
Félix Jullien <jullienfelix@gmail.com>
2021-08-18 19:44:38 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { LitElement, html} from "lit-element";
+  
+   class HanziQuizzWC extends LitElement {
+     render() {
+       return html` <div>Hello world!!</div> `;
+     }
+   }
+ customElements.define("hanzi-quizz", HanziQuizzWC);
+
