a2a52675c2db30e75a3959b3758e0a65026233e7
Félix JULLIEN <jullienfelix@gmail.com>
2024-03-02 13:44:43 +0100

    controls: add teachme btn

--- src/HanziQuiz.ts
+++ src/HanziQuiz.ts
@@ -111,6 +111,14 @@ export default class HanziQuiz extends LitElement {
     this.hanziWriter?.quiz();
   }
 
+  onTeachMe(): void {
+    this.hanziWriter?.showOutline();
+    setTimeout(() => {
+      this.hanziWriter?.hideOutline();
+      this.hanziWriter?.quiz();
+    }, 3000);
+  }
+
   onMistake(): void {
     this.rating = Math.max(1, this.rating - 1);
   }
@@ -255,6 +263,10 @@ export default class HanziQuiz extends LitElement {
             d="M16.24,3.56L21.19,8.5C21.97,9.29 21.97,10.55 21.19,11.34L12,20.53C10.44,22.09 7.91,22.09 6.34,20.53L2.81,17C2.03,16.21 2.03,14.95 2.81,14.16L13.41,3.56C14.2,2.78 15.46,2.78 16.24,3.56M4.22,15.58L7.76,19.11C8.54,19.9 9.8,19.9 10.59,19.11L14.12,15.58L9.17,10.63L4.22,15.58Z" />
         </svg>
   
+      </mwc-icon-button>
+      <mwc-icon-button @click="${this.onTeachMe}">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+        <path fill="currentColor" d="M240-80v-172q-57-52-88.5-121.5T120-520q0-150 105-255t255-105q125 0 221.5 73.5T827-615l52 205q5 19-7 34.5T840-360h-80v120q0 33-23.5 56.5T680-160h-80v80h-80v-160h160v-200h108l-38-155q-23-91-98-148t-172-57q-116 0-198 81t-82 197q0 60 24.5 114t69.5 96l26 24v208h-80Zm254-360Zm-14 120q17 0 28.5-11.5T520-360q0-17-11.5-28.5T480-400q-17 0-28.5 11.5T440-360q0 17 11.5 28.5T480-320Zm-30-128h61q0-25 6.5-40.5T544-526q18-20 35-40.5t17-53.5q0-42-32.5-71T483-720q-40 0-72.5 23T365-637l55 23q7-22 24.5-35.5T483-663q22 0 36.5 12t14.5 31q0 21-12.5 37.5T492-549q-20 21-31 42t-11 59Z"/></svg>
       </mwc-icon-button>
   
     </div>
